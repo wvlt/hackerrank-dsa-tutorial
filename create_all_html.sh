215b7162ef7c2e0ccfd7d9bbeceb2cf883c0a0ab
wvlt <amir.charkhi@outlook.com>
2025-12-13 10:31:32 +0800

    Clean up repository: remove unnecessary files
    - Remove macOS metadata files (._*)
    - Remove Jupyter checkpoint files
    - Remove template and script files
    - Add .gitignore to prevent future clutter
    - Repository now only contains essential files: notebooks and HTML tutorials

--- create_all_html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to create comprehensive interactive HTML tutorials
-# This will be executed to generate all HTML files
-
-echo "Creating comprehensive interactive HTML tutorials..."
-echo "This script will generate 6 large HTML files (~600-700 lines each)"
-echo "Files: 2_2d_array_ds.html, 3_dynamic_array.html, 4_left_rotation.html,"
-echo "       5_sparse_arrays.html, 6_print_elements_of_linked_list.html,"
-echo "       8_insert_node_at_head.html"
